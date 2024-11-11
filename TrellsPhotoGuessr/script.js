bd6b5bed786e5db289677ee12b82ec94fe3a741f
lbrughton19 <lbroughton@ucdavis.edu>
2024-11-10 19:16:22 -0500

    trying again with jpegs

--- TrellsPhotoGuessr/script.js
+++ TrellsPhotoGuessr/script.js
@@ -25,38 +25,96 @@
     
     //putting all photos into an array with latlng
     const photos = [
-        {src: 'worldImages/8T0A0007.avif', lat: -12.091318091867835, lng: -77.0858359339036, 
-        hint1:"this country is known for its appreciation of llamas",
-        hint2:"this country is known for its appreciation of llamas"}, //Circuito de playas de la Costa Verde
-        {src: 'worldImages/IMG_9879.avif', lat: 5.1037001815250695, lng: -1.2407487631389815}, //Cape Coast Sunset
-        {src: 'worldImages/IMG_7968.avif', lat: -3.353205785972626, lng: 36.860404014150845}, //Black and White Colobus Monkey in Usa River
-        {src: 'worldImages/IMG_4692.avif', lat: -3.4704, lng: 37.1865}, //Maji Moto  Hot Springs
-        {src: 'worldImages/IMG_4755.avif', lat: 6.2238, lng: -75.568}, //Medellin City Scape
-        {src: 'worldImages/IMG_4935.avif', lat: 35.5210, lng: 35.5210}, //Bones picked clean at home
-        {src: 'worldImages/IMG_5457.avif', lat: 35.6895, lng: -75.1595}, //Guatape Village
-        {src: 'worldImages/IMG_5732.avif', lat: 6.21859085470124, lng: -75.178592}, //Guatape Rock (piedra del Penol)
-        {src: 'worldImages/IMG_8025.avif', lat: -3.3582325516138027, lng: 36.903390883526}, //Usa River Roadside
-        {src: 'worldImages/IMG_8087.avif', lat: -3.346722361031439, lng: 36.89130306200241}, //Imbaseni Primary School Kids
-        {src: 'worldImages/IMG_9833.avif', lat: 5.1037001815250695, lng: -1.2407487631389815}, //Cape Coast Afternoon
-        {src: 'worldImages/IMG_9857.avif', lat: 5.103678808925347, lng: -1.2403196096965987}, //Cape Coast Canoes
-        {src: 'worldImages/IMG_1809.avif', lat: 25.28640380026731, lng: -308.4688925745286}, //Doha street art/Souq Waqif Night Market
-        {src: 'worldImages/IMG_1828.avif', lat: 25.29173267843082, lng: -308.4599018105655}, //Doha Persian Soldier Wood Engraving
-        {src: 'worldImages/IMG_4602.avif', lat: 6.196384386093956, lng: -75.5740106106896}, //Medellin Flower Festival in Sante Fe
-        {src: 'worldImages/IMG_0842.avif', lat: 6.875301514437493, lng: -1.4447593706427144}, //King in Kumasi
-        {src: 'worldImages/IMG_0235.avif', lat: 5.64536911415853, lng: -0.18849134467018305}, //Betty in Accra
-        {src: 'worldImages/8T0A0114.avif', lat: -12.046109346953921, lng: -77.02963292604181}, //Archbishop's Palace Lima
-        {src: 'worldImages/8T0A0120.avif', lat: -12.045259446084952, lng: -77.02746570115778}, //Shrine of Our Lady of SOlitude (Santuario de Nuestra Senora de Soledad)
-        {src: 'worldImages/8T0A0276.avif', lat: -13.835069277562093, lng: -76.25331580644344}, //Paracas Beach
-        {src: 'worldImages/8T0A0337.avif', lat: -13.855413905892132, lng: -76.28531456080964}, //Paracas Nazca
-        {src: 'worldImages/8T0A4585.avif', lat: 37.802341368509886, lng: -122.40583062182851}, //San Francisco Skyline
-        {src: 'worldImages/8T0A4645.avif', lat: 37.805172690644405, lng: -122.46499300024882}, //Crissy Field/ Golden Gate Park
-        {src: 'worldImages/8T0A4673.avif', lat: 47.64571602127689, lng: 122.33422636996696}, //Seattle Needle
-        {src: 'worldImages/8T0A4674.avif', lat: 47.64571602127689, lng: -122.33422636996696}, //Seattle Gas Works
-        {src: 'worldImages/8T0A5292.avif', lat: 47.606155810240736, lng: -122.34249651437494}, //Seattle Mt. Rainer
-        {src: 'worldImages/8T0A0078.avif', lat: -12.04262053920696, lng: -77.03522399069244}, //Santuario de Santa Rosa de Lima
-        {src: 'worldImages/8T0A0097.avif', lat: -12.044729568255836, lng: -77.03123062851773}, //Casa de la Gastronomica Peruana
-        {src: 'worldImages/8T0A0107.avif', lat: -12.047169096120564, lng: -77.03145682817195}, //Box Balconies of Central Lima
-        {src: 'worldImages/8T0A0109.avif', lat: -12.047022200446197, lng: -77.02983677392696}, //Lima Metropolitan Cathedral
+        {src: 'worldImages/8T0A0007.jpg', lat: -12.091318091867835, lng: -77.0858359339036, 
+            hint1:"Sunsets Abound: People flock for this iconic view all along this 20km Pacific Highway.",
+            hint2:"Stay Active: If surfing isn't your thing, nearby Costa Verde-San Miguel Park."}, //Circuito de playas de la Costa Verde
+        {src: 'worldImages/IMG_9879.jpg', lat: 5.1037001815250695, lng: -1.2407487631389815, 
+            hint1:"Complex Histories: This city's identity is deeply influenced in its colonial era, which ended in 1957",
+            hint2:"Golden Hues: This country is known as the 'Land of Gold' due to its long association with gold production and trade"}, //Cape Coast Sunset
+        {src: 'worldImages/IMG_7968.jpg', lat: -3.353205785972626, lng: 36.860404014150845, 
+            hint1:"Tucked in Treetops: Black and white colobus nkeys have been observed up to 50 feet between trees",
+            hint2:"More than Bananas: These monkeys have been known to eat the local corn crops, much to the annoyance of the local farmers (who call them 'kima')"}, //Black and White Colobus Monkey in Usa River, Tanzania
+        {src: 'worldImages/IMG_4692.jpg', lat: -3.4704, lng: 37.1865, 
+            hint1:"A Real Life Oasis?!: This natural Hot Spring near the town if Boma Ng'ombe is tucked away in the otherwise dry landscape",
+            hint2:"(Not So) Hot Springs: The waters come from the melted run off of Kilimnajaro, making for a cool swim"}, //Maji Moto  Hot Springs
+        {src: 'worldImages/IMG_4755.jpg', lat: 6.2238, lng: -75.568, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Medellin City Scape
+        {src: 'worldImages/IMG_4935.jpg', lat: 35.5210, lng: 35.5210, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Bones picked clean at home
+        {src: 'worldImages/IMG_5457.jpg', lat: 35.6895, lng: -75.1595, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Guatape Village
+        {src: 'worldImages/IMG_5732.jpg', lat: 6.21859085470124, lng: -75.178592, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Guatape Rock (piedra del Penol)
+        {src: 'worldImages/IMG_8025.jpg', lat: -3.3582325516138027, lng: 36.903390883526, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Usa River Roadside
+        {src: 'worldImages/IMG_8087.jpg', lat: -3.346722361031439, lng: 36.89130306200241, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Imbaseni Primary School Kids
+        {src: 'worldImages/IMG_9833.jpg', lat: 5.1037001815250695, lng: -1.2407487631389815, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Cape Coast Afternoon
+        {src: 'worldImages/IMG_9857.jpg', lat: 5.103678808925347, lng: -1.2403196096965987, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Cape Coast Canoes
+        {src: 'worldImages/IMG_1809.jpg', lat: 25.28640380026731, lng: -308.4688925745286, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Doha street art/Souq Waqif Night Market
+        {src: 'worldImages/IMG_1828.jpg', lat: 25.29173267843082, lng: -308.4599018105655, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Doha Persian Soldier Wood Engraving
+        {src: 'worldImages/IMG_4602.jpg', lat: 6.196384386093956, lng: -75.5740106106896, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Medellin Flower Festival in Sante Fe
+        {src: 'worldImages/IMG_0842.jpg', lat: 6.875301514437493, lng: -1.4447593706427144, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //King in Kumasi
+        {src: 'worldImages/IMG_0235.jpg', lat: 5.64536911415853, lng: -0.18849134467018305, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Betty in Accra
+        {src: 'worldImages/8T0A0114.jpg', lat: -12.046109346953921, lng: -77.0296329260418, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Archbishop's Palace Lima
+        {src: 'worldImages/8T0A0120.jpg', lat: -12.045259446084952, lng: -77.02746570115778, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Shrine of Our Lady of SOlitude (Santuario de Nuestra Senora de Soledad)
+        {src: 'worldImages/8T0A0276.jpg', lat: -13.835069277562093, lng: -76.25331580644344, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Paracas Beach
+        {src: 'worldImages/8T0A0337.jpg', lat: -13.855413905892132, lng: -76.28531456080964, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Paracas Nazca
+        {src: 'worldImages/8T0A4585.jpg', lat: 37.802341368509886, lng: -122.40583062182851, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //San Francisco Skyline
+        {src: 'worldImages/8T0A4645.jpg', lat: 37.805172690644405, lng: -122.46499300024882, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Crissy Field/ Golden Gate Park
+        {src: 'worldImages/8T0A4673.jpg', lat: 47.64571602127689, lng: 122.33422636996696, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Seattle Needle
+        {src: 'worldImages/8T0A4674.jpg', lat: 47.64571602127689, lng: -122.33422636996696, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Seattle Gas Works
+        {src: 'worldImages/8T0A5292.jpg', lat: 47.606155810240736, lng: -122.34249651437494, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Seattle Mt. Rainer
+        {src: 'worldImages/8T0A0078.jpg', lat: -12.04262053920696, lng: -77.03522399069244, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Santuario de Santa Rosa de Lima
+        {src: 'worldImages/8T0A0097.jpg', lat: -12.044729568255836, lng: -77.03123062851773, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Casa de la Gastronomica Peruana
+        {src: 'worldImages/8T0A0107.jpg', lat: -12.047169096120564, lng: -77.03145682817195, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Box Balconies of Central Lima
+        {src: 'worldImages/8T0A0109.jpg', lat: -12.047022200446197, lng: -77.02983677392696, 
+            hint1:"this country is known for its appreciation of llamas",
+            hint2:"this country is known for its appreciation of llamas"}, //Lima Metropolitan Cathedral
 
 
 
@@ -146,11 +204,11 @@
         
             if (distance>500 && guessCount===1){
                 document.querySelector('#result').innerHTML = `Try again! You are ${Math.round(distance)} miles from the location.`;  
-                hint1.innerHTML=  `Hint 1: ${photos[randomIndex].hint1}`;        
+                // hint1.innerHTML=  `Hint 1: ${photos[randomIndex].hint1}`;        
                 }
             else if (distance>500 && guessCount===2){
                 document.querySelector('#result').innerHTML = `One More try! You are ${Math.round(distance)} miles from the location.`;
-                hint2.innerHTML= `Hint 2: ${photos[randomIndex].hint2}`;
+                // hint2.innerHTML= `Hint 2: ${photos[randomIndex].hint2}`;
             }
 
             else{
